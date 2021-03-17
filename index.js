c0d8bcbe00fc35acc2c63e06a5d9fecffd774c62
Shivam Tiwari <shivamtiwari12032001@gmail.com>
2021-03-17 14:53:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const inputs =document.querySelectorAll(".controls input");
+function handleUpadte(){
+    // console.log(this.value);
+    const suffix=this.dataset.sizing || '';
+    // console.log(this.dataset);
+    // console.log(suffix);
+    // console.log(this.name)
+    document.documentElement.style.setProperty(`--$(this.name)`,this.value+suffix);
+}
+inputs.forEach(input=> input.addEventListener('change',handleUpadte));
+inputs.forEach(input=> input.addEventListener('mousemove',handleUpadte));
